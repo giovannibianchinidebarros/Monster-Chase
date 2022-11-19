38ded5ae9130ff8cfa046e3b8cff9435f18399d9
giovannibianchinidebarros <giovannibarros@hotmail.com>
2022-11-19 13:20:52 -0300

    added player movement and jump

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -16,6 +16,10 @@ public class Player : MonoBehaviour
 
     private string walk_animation = "Walk";
 
+    private float movementX;
+    private bool isGrounded = true;
+    private string ground_tag = "Ground";
+
     private void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
@@ -32,6 +36,57 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+        PlayerJump();
+    }
+
+    void FixedUpdate()
+    {
 
     }
-}
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer()
+    {
+        if (movementX > 0)
+        {
+            anim.SetBool(walk_animation, true);
+            sr.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            anim.SetBool(walk_animation, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            anim.SetBool(walk_animation, false);
+        }
+
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(ground_tag))
+        {
+            isGrounded = true;
+        }
+    }
+
+
+} // class Player
